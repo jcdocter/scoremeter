076d10862dad1181fad0d36671fb020b1b20d52f
blies <suri-boy1@live.nl>
2018-11-13 10:51:04 +0100

    test

--- resources/views/players.blade.php
+++ resources/views/players.blade.php
@@ -5,6 +5,25 @@
         <div class="row ">
             <div class="col">
                 <div id="Gryffindor"></div>
+                <script type="text/javascript">
+                    var capnum = 0;
+                    function increment(){
+                        capnum++;
+                        document.getElementById('display').innerHTML = capnum;
+                    }
+                </script>
+                <button onclick="increment()">+</button>
+
+                <script type="text/javascript">
+                    var capnum = 0;
+                    function decrement(){
+                        capnum--;
+                        document.getElementById('display').innerHTML = capnum;
+                    }
+                </script>
+                <button onclick="decrement()">-</button>
+                <div id="display"><script type="text/javascript">document.write(capnum);</script></div>
+            </div>
             </div>
             <div class="col">
                 <div id="Slytherin"></div>
