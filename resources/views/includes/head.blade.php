286f2eaa390dc8a40959e39d2a24e7262b541e16
ViscaMB <b_mo@live.nl>
2018-11-13 11:40:59 +0100

    inlog pagina achtergrond en kleur veranderd naar huisstyle

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,3 +6,4 @@
     <link href="{{asset ('css/nav.css') }}" rel="stylesheet" type="text/css">
     <link href="{{asset ('css/main.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    {{--<link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8" />--}}
