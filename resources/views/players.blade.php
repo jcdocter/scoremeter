b15c3adade6e8cfd6c6f9a4923f38613bd9c3a09
blies <suri-boy1@live.nl>
2018-11-23 14:41:34 +0100

    img

--- resources/views/players.blade.php
+++ resources/views/players.blade.php
@@ -1,15 +1,62 @@
 @extends('layouts.players')
 
 @section('content')
-<div class="container">
+    <style>
+
+img{
+    width: 50%;
+    display: inline-block;
+
+
+}
+
+        *{
+            box-sizing: border-box;
+        }
+
+        .col {
+            float: left;
+            width: 25%;
+            padding: 5px;
+        }
+
+        /* Clearfix (clear floats) */
+        .row::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
+
+        button{
+            display: inline-block;
+            background-color: red;
+            width: 14%;
+
+
+        }
+        .row{
+            display: inline;
+
+        }
+
+
+    </style>
+
+
+
+    <div class="container">
         <div class="row ">
+
+
             <div class="col">
                 <div id="Gryffindor"></div>
+                <img src="images/gryffindor.jpg" />
                 <script type="text/javascript">
                     var capnum = 0;
                     function increment(){
                         capnum++;
                         document.getElementById('display').innerHTML = capnum;
+
                     }
                 </script>
                 <button onclick="increment()">+</button>
@@ -22,22 +69,87 @@
                     }
                 </script>
                 <button onclick="decrement()">-</button>
-                <div id="display"><script type="text/javascript">document.write(capnum);</script></div>
+                <div id="display" ><script type="text/javascript">document.write(capnum);</script></div>
+
+
+
             </div>
-            </div>
+
             <div class="col">
                 <div id="Slytherin"></div>
-            </div>
+                <img src="images/hufflepuff.jpg" />
+
+                <script type="text/javascript">
+            var capnumh = 0;
+            function incrementh(){
+                capnumh++;
+                document.getElementById('displayh').innerHTML = capnumh;
+            }
+                </script>
+                <button onclick="incrementh()" >+</button>
+
+                <script type="text/javascript">
+                    var capnumh = 0;
+                    function decrementh(){
+                        capnumh--;
+                        document.getElementById('displayh').innerHTML = capnumh;
+                    }
+                </script>
+                <button onclick="decrementh()">-</button>
+                <div id="displayh" ><script type="text/javascript">document.write(capnumh);</script></div>
+
+
+
+                </div>
             <div class="col">
                <div id="Hufflepuff"></div>
+                <img src="images/ravenclaw.jpg"/>
+                <script type="text/javascript">
+                    var capnumr = 0;
+                    function incrementr(){
+                        capnumr++;
+                        document.getElementById('displayr').innerHTML = capnumr;
+                    }
+                </script>
+                <button onclick="incrementr()" >+</button>
+
+                <script type="text/javascript">
+                    var capnumr = 0;
+                    function decrementr(){
+                        capnumr--;
+                        document.getElementById('displayr').innerHTML = capnumr;
+                    }
+                </script>
+                <button onclick="decrementr()">-</button>
+                <div id="displayr" ><script type="text/javascript">document.write(capnumr);</script></div>
             </div>
             <div class="col">
               <div id="Ravenclaw"></div>
+                <img src="images/slytherin.jpg" />
+                <script type="text/javascript">
+                    var capnums = 0;
+                    function increments(){
+                        capnums++;
+                        document.getElementById('displays').innerHTML = capnums;
+                    }
+                </script>-
+                <button onclick="increments()" >+</button>
+
+                <script type="text/javascript">
+                    var capnums = 0;
+                    function decrements(){
+                        capnums--;
+                        document.getElementById('displays').innerHTML = capnums;
+                    }
+                </script>
+                <button onclick="decrements()">-</button>
+                <div id="displays" ><script type="text/javascript">document.write(capnums);</script></div>
             </div>
-        </div>
+
+    </div>
+    </div>
     </div>
 
+    <script src="{{mix('js/app.js')}}"></script>
 
-
-<script src="{{mix('js/app.js')}}"></script>
 @endsection
